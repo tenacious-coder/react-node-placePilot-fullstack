b2fbf29a29d7f5f418c1f1c5be239a6acfcc89e9
Anjali <anjalikumari70996@gmail.com>
2025-10-25 21:40:38 +0530

    error corrected

--- frontend/placePilot/src/pages/Home.jsx
+++ frontend/placePilot/src/pages/Home.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import FeaturesSection from "../components/FeaturesSection";
 import PlacementStats from "../components/PlacementStats";
-import DashboardCards from "../components/DashBoard";
+import DashboardCards from "../components/Dashboard";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  useGetAllJobs();
+  const { user } = useSelector(store => store.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.role === 'recruiter') {
+      navigate("/admin/companies");
+    }
+  }, []);
   return (
     <div>
       {/* Hero / Stats / Features */}
