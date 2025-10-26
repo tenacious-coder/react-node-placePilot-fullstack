de0508a5bbced5f2e7590a55ba7daf851203c467
Anjali <anjalikumari70996@gmail.com>
2025-10-26 15:10:19 +0530

    fed up

--- backend/config/datauri.js
+++ backend/config/datauri.js
@@ -3,6 +3,7 @@ import DataUriParser from "datauri/parser.js"
 import path from "path";
 
 const getDataUri = (file) => {
+    if(!file) return null;
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer);
